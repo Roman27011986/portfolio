54c6cc4f208fcc130b01678ac4a04a00e6f58408
Roman <didorsn@gmail.com>
2021-09-18 17:47:52 +0300

    qe

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,34 @@
 import styles from './Home.module.scss'
 import myphoto from '../../images/myphoto.jpg'
+import beach from '../../video/Computer.mp4'
+import Typical from 'react-typical'
 export default function Home() {
     return (
         <>
-        <div className={styles.homeContentContainer}>
-            <div>
-                <h1 className={styles.title}>Hi there!</h1>
-                <p className={styles.text}>My name's Roman and I’m a Front-end Developer!</p>
+            <div className={styles.homeContentContainer}>
+            <div className={styles.videoBox}>
+                        <Typical
+                            className={styles.text}
+                        steps={['Hi there!', 1000,
+                            'My names Roman and I аm a Front end Developer!', 1000,
+                            'I like teamwork.', 1000,
+                            'I аm ambitious.', 1000,
+                            'I аm creative.', 1000,
+                            'I аm disciplined.',1000
+                        ]}
+                         loop={Infinity}
+                         wrapper="p"
+                         />
+                    <video src={beach} className={styles.video}  autoPlay loop playsInline muted preload='auto'></video>
             </div>
             <div className={styles.imgBox}>
                 <img className={styles.mainImg} src={myphoto} alt="" />
             </div>
             </div>
             <div className={styles.contactsBox}>
+                
                 <h2 className={styles.title}>Tech Skills:</h2>
+               
                 <ul className={styles.techSkills}>
                     <li className={styles.techSkillsItem}><div className={styles.iconhtml}></div><span className={styles.techSkillsText}>HTML5</span></li>
                     <li className={styles.techSkillsItem}><div className={styles.iconcss}></div><span className={styles.techSkillsText}>CSS3</span></li>
